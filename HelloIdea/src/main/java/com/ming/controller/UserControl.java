d4e190924694473705f8fe74e46b571dd28e1780
yanming0924 <473916147@qq.com>
2018-05-04 11:35:45 +0800

    修改UserControl

--- HelloIdea/src/main/java/com/ming/controller/UserControl.java
+++ HelloIdea/src/main/java/com/ming/controller/UserControl.java
@@ -5,7 +5,10 @@ import com.ming.service.MyUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -15,19 +18,25 @@ import java.util.List;
  * Created by Administrator on 2018/4/27.
  */
 @Controller
+@RequestMapping("/user")
 public class UserControl {
     @Autowired
     private MyUserService myUserService;
-    @RequestMapping("/user/byName")
+    @RequestMapping("/byName")
     public MyUser getUserByName(){
         return myUserService.getUserByName();
     }
 
     @RequestMapping("/find")
     public String find(ModelMap model, HttpServletRequest request){
-        List<MyUser> deptList=myUserService.findAll();
+        List<MyUser> myUsers=myUserService.findAll();
         //将获取到的数据存入ModelMap中，以便页面取出
-        model.addAttribute("depts", deptList);
+        model.addAttribute("myUsers", myUsers);
         return "dept";
+    }
+    @RequestMapping(value = "/update",method = RequestMethod.GET)
+    @ResponseBody
+    public String updateMyUser(){
+        return "success";
     }
 }
